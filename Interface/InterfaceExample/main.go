e71ba267f21b0fec12072522823d193bf7a79b8c
Aayu3248 <aayush@ini8labs.tech>
2023-03-14 13:34:08 +0530

    refactor: changed variable to local

--- Interface/InterfaceExample/main.go
+++ Interface/InterfaceExample/main.go
@@ -3,23 +3,23 @@ package main
 import "github.com/ini8labs/Go-Learning/Interface/shapes"
 
 func main() {
-	Cube := shapes.Cube{Side: 10}
+	cube := shapes.Cube{Side: 10}
 
-	Cuboid := shapes.Cuboid{
+	cuboid := shapes.Cuboid{
 		Length:  3,
 		Breadth: 5,
 		Height:  10,
 	}
 
-	Cylinder := shapes.Cylinder{
+	cylinder := shapes.Cylinder{
 		Radius: 10,
 		Height: 20,
 	}
 
-	Sphere := shapes.Sphere{
+	sphere := shapes.Sphere{
 		Radius: 10,
 	}
 
-	object := []shapes.Shape{Cube, Cuboid, Cylinder, Sphere}
+	object := []shapes.Shape{cube, cuboid, cylinder, sphere}
 	shapes.Calculate(object)
 }
