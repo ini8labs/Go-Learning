0f760d3f3b89a9c0a03542dbf20a1dc063c3db99
Mohit Kangorta <mohit@ini8labs.tech>
2023-03-17 12:56:40 +0530

    feat: sucsessful build  main_test.go

--- Interface/main_test.go
+++ Interface/main_test.go
@@ -7,24 +7,27 @@ import (
 )
 
 func Test_totalExpense(t *testing.T) {
-	type args struct {
-		s []catalogue.Price
+
+	bmw := catalogue.Beemer{
+		Name:      "BMW",
+		Yom:       2012,
+		BasePrice: 10,
 	}
 	tests := []struct {
-		name string
-		inpt args
-		outpt float64
-	},{
-		name: "beemer1",
-		inpt: args.s.PriceCalculator{Name:"BMW",
-		Yom:2020,
-		BasePrice:300000}, 
-		outpt: 273000
-	 },
+		name  string
+		input []catalogue.Price
+		want  float64
+	}{
+		{
+			name:  "test",
+			input: []catalogue.Price{bmw},
+			want:  6.7,
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if inpr := totalExpense(tt.args.s); inpt != tt.outpt {
-				t.Errorf("totalExpense() = %v, want %v", inpt, tt.outpt)
+			if got := totalExpense(tt.input); got != tt.want {
+				t.Errorf("totalExpense() = %v, want %v", got, tt.want)
 			}
 		})
 	}
