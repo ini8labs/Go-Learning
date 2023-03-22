9a2b538ec78366c7b979d0a8cc01f3ee6eb87fd2
Mohit Kangorta <mohit@ini8labs.tech>
2023-03-22 16:28:00 +0530

    refactor: updated mod file

---
+++ Swagger/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	docs "github.com/ini8labs/Go-Learning/Swagger"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "helloworld")
+}
+
+func SampleResponse(g *gin.Context) {
+	g.JSON(http.StatusOK, "this is sample response")
+}
+
+func main() {
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", Helloworld)
+			eg.GET("sample", SampleResponse)
+
+		}
+
+	}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run(":8080")
+
+}
